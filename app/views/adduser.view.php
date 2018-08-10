7d40e356670894d78241014f0489a575efb26d7d
Federico Zanellato <federico.zanellato@studenti.unipd.it>
2018-08-10 16:53:27 +0200

    Test grafici, sistemo stasera

--- app/views/adduser.view.php
+++ app/views/adduser.view.php
@@ -1,26 +1,43 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link type="text/css" rel="stylesheet" href="./../../../public/css/style.css" />
+  <title lang="en">Walk & Buy</title>
+
+</head>
+<body class="public">
 
 <form action="adduser" method='post'>
-
-  <label class="input-text" for="nome">Cognome</label>
-  <input id="cognome"  type="text" name="cognome" required />
-
-  <label class="input-text" for="nome">Nome</label>
-  <input id="nome"  type="text" name="name" required />
-
-  <label class="input-text" for="nome">Codice Fiscale</label>
-  <input id="codicefiscale"  type="text" name="cf" required />
-
-  <label class="input-text" for="nome">Telefono</label>
-  <input id="telefono"  type="text" name="telefono" required />
-
-  <label class="input-text" for="nome">Email</label>
-  <input id="email"  type="text" name="email" required />
-
-
-  <button type="submit">Conferma</button>
-
-
-
-
+  
+<div class="loginbox-field">    
+  <label class="input-label" for="nome">Email</label>
+  <input class="input" id="email"  type="text" name="email" required />
+  <span class="underline"></span>
+</div>
+<div class="loginbox-field">  
+  <label class="input-label" for="nome">Cognome</label>
+  <input class="input" id="cognome"  type="text" name="cognome" required />
+  <span class="underline"></span>
+</div>
+<div class="loginbox-field">  
+  <label class="input-label" for="nome">Nome</label>
+  <input class="input" id="nome"  type="text" name="name" required />
+  <span class="underline"></span>
+</div>
+<div class="loginbox-field">  
+  <label class="input-label" for="nome">Codice Fiscale</label>
+  <input class="input" id="codicefiscale"  type="text" name="cf" required />
+  <span class="underline"></span>
+</div>
+<div class="loginbox-field">  
+  <label class="input-label" for="nome">Telefono</label>
+  <input class="input" id="telefono"  type="text" name="telefono" required />
+  <span class="underline"></span>
+</div>
+  <button type="submit" class="btn btn-outline">Conferma</button>
 
 </form>
+<?php require('partials/footer.php');?>
