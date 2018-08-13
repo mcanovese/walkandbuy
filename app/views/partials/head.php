63c02a7b50f6e0118a74804d9ab0014c6f5255ec
Federico Zanellato <federico.zanellato@studenti.unipd.it>
2018-08-13 21:51:34 +0200

    Roba css

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -4,28 +4,29 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link type="text/css" rel="stylesheet" href="./../../../public/css/style.css" />
+  <link type="text/css" rel="stylesheet" href="./../../../public/css/normalize.css" />
+  <link type="text/css" rel="stylesheet" href="./../../../public/css/common.css" />
   <title lang="en">Walk & Buy</title>
 
 </head>
 <body class="public">
-  <header class="navbar-container"> <!-- Per il back to top -->
+  <header id="top" class="navbar-container"> <!-- Per il back to top -->
     <div class="navbar container clearfix">
       <div class="navbar-left left">
         <a class="home-link" href="./" title="Ritorna alla pagina principale">
-          <img lang="en" class="logo-img" src="./../../../../img/logo6.png" alt="Logo Walk & Buy, rimanda alla home" />
+          <img lang="en" class="logo-img" src="./../../../public/img/logo6.png" alt="Logo Walk & Buy, rimanda alla home" />
         </a>
       </div>
 
-      <ul class="list navbar-center center">
+      <ul class="list navbar-right right">
         <li class="list-item">
-        <a class="navbar-littlelink" href="./howWork">Come funziona</a>
+        <a class="navbar-link" href="./howWork">Come funziona</a>
         </li>
         <li class="list-item">
-        <a class="navbar-littlelink" href="./farmer">Agricoltori</a>
+        <a class="navbar-link" href="./farmer">Agricoltori</a>
         </li>
         <li class="list-item">
-        <a class="navbar-littlelink" href="./whoAreWe">Chi siamo</a>
+        <a class="navbar-link" href="./whoAreWe">Chi siamo</a>
         </li>
       </ul>
 
