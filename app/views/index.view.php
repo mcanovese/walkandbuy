20f064b69cd6cb60ecd8b4f6d4639c24ccea7797
mcanovese <marco.canovese@studenti.unipd.it>
2018-08-14 16:45:10 +0200

    Auto stash before merge of "master" and "origin/master"

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,13 +1,4 @@
 <?php require('partials/head.php');?>
-<h1>Index</h1>
-
-<h1>Submit Your Name</h1>
-<form method="GET" action="/names">
-
-<?php var_dump($articoli); ?>
-  <input name="name"></input>
-  <button type="submit">Submit</button>
-
-</form>
+<h1>Walk&Buy Home</h1>
 
 <?php require('partials/footer.php');?>
