fafb301e6e645abfdb3c037cad96ea744cd69bd6
Federico Zanellato <federico.zanellato@studenti.unipd.it>
2018-08-28 15:20:50 +0200

    CSS sistemati dettagli

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href='https://fonts.googleapis.com/css?family=Amatic SC' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
   <link type="text/css" rel="stylesheet" href="./../../../public/css/normalize.css" />
   <link type="text/css" rel="stylesheet" href="./../../../public/css/common.css" />
   <link type="text/css" rel="stylesheet" href="./../../../public/css/<?= $routeName ?>.css" />
