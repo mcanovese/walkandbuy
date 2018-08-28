3c90382112b75faac51cf915ce1ac8750869cddc
mcanovese <marco.canovese@studenti.unipd.it>
2018-08-28 16:35:16 +0200

    show div in category

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -8,7 +8,7 @@
     <li><a href="/item?cod=1"> "/item?cod=1" Pagina View singolo articolo</a> (puoi visualizzarei l var dump dell'array di cui devi effettuare l'output)</li>
     <li><a href="/item?req=newItem"> "/item?req=newItem" - FORM inserimento nuovo articolo"</a> (da aggiustare alcuni campi, tipo inserire select per unit&agrave misura, ma intanto sistema css)</li>
     <li>Da fare anche view pagina 404 - file già predisposto e indirizzato su routes</li>
-
+    <li>pagina categoria singola: /category da sistemare output div</li>
   </lu>
 
 </div>
