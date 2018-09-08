fffc6225a5a568fa8a81e59037543f6817758d79
Federico Zanellato <federico.zanellato@studenti.unipd.it>
2018-09-08 15:34:46 +0200

    Css profilo

--- app/views/users.view.php
+++ app/views/users.view.php
@@ -2,24 +2,22 @@
 <?php require('partials/head.php');?>
 <div id="body">
 <h1>Profilo Utente</h1>
-</div>
+
 <div class="content">
-<?php
-echo $user->idutente;
-?>
+    <div class="user-data">
+    <label class="label-detail">Nome:</label>
+    <div class="data"> <?php echo $user->nome; ?> </div>
+    <label class="label-detail">Cognome: </label>
+    <div class="data"><?php echo $user->cognome; ?> </div>
+    <label class="label-detail">CF: </label>
+    <div class="data"><?php echo $user->cf; ?></div>
+    <label class="label-detail">Telefono: </label> 
+    <div class="data"><?php echo $user->telefono; ?></div>
+    <label class="label-detail">E-mail: </label> 
+    <div class="data"><?php echo $user->email; ?></div>
 </div>
-
-<?php
-echo $user->cognome;
-echo $user->nome;
-echo $user->cf;
-echo $user->telefono;
-echo $user->email;
-
-
-?>
-
-
+</div>
+</div>
 
 
 <?php require('partials/footer.php');?>
