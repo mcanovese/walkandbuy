29e10a1ddcc79b22a1f2def50214e80defb0cf62
Federico Zanellato <federico.zanellato@studenti.unipd.it>
2018-09-13 21:05:36 +0200

    Coap

--- app/views/partner.view.php
+++ app/views/partner.view.php
@@ -1,7 +1,7 @@
 <div id="container">
 <?php require('partials/head.php');?>
 <div id="body">
-<h1>I nostri Partner</h1>
+<h1 class="title">I nostri Partner</h1>
 
 <p> Qui vanno messi a "griglia" tutti i negozianti, con relativi dettagli brevi</p>
 </div>
