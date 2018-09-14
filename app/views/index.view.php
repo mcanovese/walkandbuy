30ef24ebba1bd8cd2d524ed2655842e3c4889047
mcanovese <marco.canovese@studenti.unipd.it>
2018-09-14 10:05:57 +0200

    final push

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -2,12 +2,18 @@
 <?php require('partials/head.php');?>
 <div id="body">
 <h1>Walk&Buy Home</h1>
+
+<img lang="it"  id="myImg" src="./../../../public/img/contadino.png" alt="Immagine contadino" />
 <p>
   Benvenuto in Walk&Buy, il primo portale ecommerce realizzato dal "Consorzio Agricoltori del Camposampierese" con l'obiettivo
   di creare una vetrina virtuale per tutti i coltivatori diretti e commercianti del nostro territorio.
   Dopo esserti autenticato potrai effettuare ordini che ti saranno recapitati direttamente a casa dal nostro servizio di consegna convenzionato
   al fine di permetterti di acquistare quotidianamente e con semplicità i prodotti della tua terra
-
+</p>
+<div class="title">
+<h2>Dal produttore al consumatore, la nostra filiera &egrave la nostra forza</h2>
+<img lang="it"  id="myImg" src="./../../../public/img/filiera.gif" alt="Immagine contadino" />
+</div>
 <div> </div>
 </div>
 <?php require('partials/footer.php');?>
--- app/views/signIn.view.php
+++ app/views/signIn.view.php
@@ -19,13 +19,13 @@
 <div class="loginbox">
 <div class="loginbox-field">
   <label class="input-label" for="email">Email</label>
-  <input class="input" id="email"  type="text" name="email" required />
+  <input class="input" id="email"  type="text" name="email" required='required' />
   <span class="underline"></span>
 </div>
 
 <div class="loginbox-field">
   <label class="input-label" for="password">Password</label>
-  <input class="input" id="password"  type="password" name="password" required />
+  <input class="input" id="password"  type="password" name="password" required='required'/>
   <span class="underline"></span>
 </div>
   <button type="submit" class="btn btn-outline">Login</button>
